5aff77c2d0132b8c7838a56ad8236fdb4bd2da0c
Maryamjbl <maryamjabali111@gmail.com>
2025-09-15 20:34:01 +0530

    commit

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+let n = 676;
+let ncopy = n;
+
+let reverse = 0;
+let remainder = 0;
+
+while (n > 0) {
+  remainder = n % 10;
+  reverse = reverse * 10 + remainder;
+  n = Math.floor(n / 10);
+}
+console.log("Reverse =" + reverse);
+
+if (ncopy === reverse) {
+  console.log("It is a Palindrome");
+} else {
+  console.log("It is not a Palindrome");
+}
