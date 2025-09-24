1445f75af3135d79ded21a4b5eda4e87ece006c3
Maryamjbl <maryamjabali111@gmail.com>
2025-09-24 23:17:05 +0530

    commit

---
+++ assignment/array.js
@@ -0,0 +1,18 @@
+let fruits = ["Apple", "Banana", "Orange"];
+
+fruits[1] = "Mango";
+
+fruits.push("Pineapple", "Grapes");
+
+let lastFruit = fruits.pop();
+console.log("Last fruit removed:", lastFruit);
+
+console.log("Total number of fruits:", fruits.length);
+
+let x;
+console.log("Fruits");
+
+for (let i = 0; i < fruits.length; i++) {
+  x = i + 1;
+  console.log("Fruits #" + x + ":" + fruits[i]);
+}
