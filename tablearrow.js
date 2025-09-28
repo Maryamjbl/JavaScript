f8b3b320ace1838241b74e1a9fef2a0565a9b10b
Maryamjbl <maryamjabali111@gmail.com>
2025-09-28 19:54:08 +0530

    commit

---
+++ tablearrow.js
@@ -0,0 +1,8 @@
+for (let n = 1; n <= 20; n++) {
+  console.log("Table of " + n);
+  for (let i = 1; i <= 10; i++) {
+    let result = n * i;
+    console.log(`${n} x ${i} = ${result}`);
+    console.log();
+  }
+}
