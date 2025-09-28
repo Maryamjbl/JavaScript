4ed18f3f9078753f4d6f63c6530762428b777dd2
Maryamjbl <maryamjabali111@gmail.com>
2025-09-28 19:49:23 +0530

    commit

---
+++ sumarrow.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+  let sum = x + y;
+  result = sum;
+}
+let addarrow = (x, y) => {
+  let sum = x + y;
+  result = sum;
+};
+let x = 10;
+let y = 15;
+let total = addarrow(x, y);
+console.log(`Sum Of ${x} and ${y} = ${total}`);
