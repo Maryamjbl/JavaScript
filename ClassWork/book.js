2eefa86e231e4b8b1fe8edc2881f8d60c8aa5377
Maryamjbl <maryamjabali111@gmail.com>
2025-10-07 11:25:27 +0530

    commit

---
+++ ClassWork/book.js
@@ -0,0 +1,24 @@
+class Book {
+  title;
+  author;
+  genre;
+  year;
+  pages;
+
+  constructor(t, a, g, y, p) {
+    this.title = t;
+    this.author = a;
+    this.genre = g;
+    this.year = y;
+    this.pages = p;
+  }
+  printDetails() {
+    console.log(
+      `"${this.title}"is written by ${this.author}, genre:${this.genre}, published in ${this.year},${this.pages} pages`
+    );
+  }
+}
+
+const book1 = new Book("Atomic Hbaits", "James Clear", "Self-Help", 2018, 320);
+
+book1.printDetails();
--- assignment/course.js
+++ assignment/course.js
@@ -10,18 +10,36 @@ class Course {
     this.fee = f;
     this.description = desc;
   }
+
   printDetails() {
     console.log("******* Course Details ********");
     console.log(`Title - ${this.title}`);
     console.log(`Duration - ${this.duration}`);
     console.log(`Fee - ${this.fee}`);
     console.log(`Description - ${this.description}`);
-    console.log("*******************************");
+    console.log("*******************************\n");
   }
 }
-const course1 = new Course("MERN Stack", "120 Days", 18000, "Mongo DB, Express JS, React JS, Node JS");
-const course2 = new Course("Python Full Stack", "100 Days", 15000, "Python, Django, MySQL, React");
-const course3 = new Course("AWS Cloud Practitioner", "45 Days", 12000, "AWS Basics, EC2, S3, IAM, Cloud Concepts");
+
+const course1 = new Course(
+  "MERN Stack",
+  "120 Days",
+  18000,
+  "Mongo DB, Express JS, React JS, Node JS"
+);
+const course2 = new Course(
+  "Python Full Stack",
+  "100 Days",
+  15000,
+  "Python, Django, MySQL, React"
+);
+
+const course3 = new Course(
+  "AWS Cloud Practitioner",
+  "45 Days",
+  12000,
+  "AWS Basics, EC2, S3, IAM, Cloud Concepts"
+);
 
 course1.printDetails();
 course2.printDetails();
