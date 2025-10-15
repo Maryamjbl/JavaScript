dc7bd10238fbbfc4ac5cbae7a27263adab016316
Maryamjbl <maryamjabali111@gmail.com>
2025-10-15 19:47:32 +0530

    commit

---
+++ ClassWork/name-using.mjs
@@ -0,0 +1,3 @@
+import square from "./named.mjs";
+let n = 4;
+console.log(`Square of ${n} = ${square(n)}`);
---
+++ ClassWork/named.mjs
@@ -0,0 +1,6 @@
+let square = (n) => n * n;
+
+
+// export { square, factorial };
+export default square;
+// export {factorial}
