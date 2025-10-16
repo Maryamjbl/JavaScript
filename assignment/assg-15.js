09c63930c1a698cb7439caadf337309edd9983f0
Maryamjbl <maryamjabali111@gmail.com>
2025-10-16 12:13:38 +0530

    commit

--- assignment/assg-15.js
+++ assignment/assg-15.js
@@ -8,7 +8,7 @@ fruits.forEach(fruit => {
 })
 let number = [10, 20, 30, 40, 50];
 number.forEach(n => {
-    console.log(n* n);
+    console.log(`Square of ${n} = ${n * n}`);
 });
 console.log("*********************");
 
@@ -27,3 +27,36 @@ let totalMarks = students.reduce((sum, student) => sum + student.marks, 0);
 let averageMarks = totalMarks / students.length;
 
 console.log(`Average marks: ${averageMarks}`);
+console.log();
+
+let frontend = ["HTML", "CSS", "JavaScript"];
+let backend = ["Node.js", "Express", "MongoDB"];
+
+let fullStack = [...frontend, ...backend];
+let Combinedarray = [...fullStack, "React"];
+
+console.log("Frontend-", frontend);
+console.log("Backend-", backend);
+console.log("Full Stack-", fullStack);
+console.log(" Updated Array With React-", Combinedarray);
+console.log();
+
+let student = { name: "Aysha", age: 21 };
+let studentDetails = {
+  ...student,
+  course: "Full Stack Development",
+  city: "Manglore"
+};
+
+console.log("Student:", student);
+console.log("Student Details:", studentDetails);
+console.log();
+
+let colors = ["Red", "Green", "Blue"];
+let newColors = [...colors];
+newColors.push("Yellow");
+
+console.log("Original Colors:", colors);
+console.log("Copied Colors:", newColors);
+
+
