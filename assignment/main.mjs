b625bbc9256117a1bdec2a5d08fdc1ff2beca14b
Maryamjbl <maryamjabali111@gmail.com>
2025-10-19 10:18:23 +0530

    commit

--- assignment/main.mjs
+++ assignment/main.mjs
@@ -1,10 +1,10 @@
 import { add, subtract, multiply, divide } from './mathutils.mjs';
 
 
-let a = 8;
-let b = 2;
+let a = 6;
+let b = 4;
 
 console.log('Addition:', add(a, b));         
 console.log('Subtraction:', subtract(a, b)); 
 console.log('Multiplication:', multiply(a, b)); 
-console.log('Division:', divide(a, b));     
+console.log('Division:', divide(8, 4));     
