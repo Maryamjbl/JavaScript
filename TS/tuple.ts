fa22a1f7091db21a963648350a9a36ed5ad1c7f0
Maryamjbl <maryamjabali111@gmail.com>
2025-10-27 20:59:42 +0530

    commit

--- TS/tuple.ts
+++ TS/tuple.ts
@@ -1,10 +1,6 @@
-type user = [number, string];
-
-let user1: user = [1, "Aysha"];
-let user2: user = [2, "Ameena"];
-
-console.log(user2[0]); // 1
-console.log(user2[1]); // "Alice"
-
-let product: [number, string, number] = [1238, "HP Pavillion Laptop", 100000];
+var user1 = [1, "Aysha"];
+var user2 = [2, "Ameena"];
+console.log(user2[0]); 
+console.log(user2[1]); 
+var product = [1238, "HP Pavillion Laptop", 100000];
 console.log(product[1]);
