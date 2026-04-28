b3f4819e640e8310b6e766dd4660f0403b39ef42
Maryamjbl <maryamjabali111@gmail.com>
2026-04-28 23:04:28 +0530

    commit

--- TS/rev/interface.ts
+++ TS/rev/interface.ts
@@ -1,19 +1,23 @@
-interface userName {
+interface User {
   name: string;
   email: string;
   age: number;
   bio?: string;
 }
 
-let user1: userName = {
+let user1: User = {
   name: "Maryam Jabali",
   email: "maryamjbl@gmail.com",
   age: 16,
   bio: "I am a student and I love programming.",
 };
-let user2: userName = {
+
+let user3: User = {
   name: "Ali",
   email: "ali@gmail.com",
   age: 20,
 };
-console.log(user.email);
+
+console.log(user1.email);  
+console.log(user3.name);    
+console.log(user3.bio);
